f59e9512283592d4fea67a953b8bb3bb65e2ea5d
nino420 <riadhyous@gmail.com>
2021-06-24 20:53:31 -0500

    Updates

---
+++ precache-manifest.5fe993c919aa27bc03d90f8db2ac730b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53ce5659f823367dd11980ebaf89c6d",
+    "url": "/react-deploy.git/index.html"
+  },
+  {
+    "revision": "e7ee0ee63c3090bceebd",
+    "url": "/react-deploy.git/static/css/main.c34a4f03.chunk.css"
+  },
+  {
+    "revision": "72cf42a21d9c9b9473de",
+    "url": "/react-deploy.git/static/js/2.d0829338.chunk.js"
+  },
+  {
+    "revision": "e7ee0ee63c3090bceebd",
+    "url": "/react-deploy.git/static/js/main.c840f6e5.chunk.js"
+  },
+  {
+    "revision": "7ad606e794b480368ead",
+    "url": "/react-deploy.git/static/js/runtime~main.34c0aa00.js"
+  }
+]);
